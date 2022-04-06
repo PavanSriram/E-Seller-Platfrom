978004ca263fc35cae1c7367081ab21f8623823c
Sriram Gudipati <pavangud9@gmail.com>
2022-04-06 15:47:29 +0530

    intial setup[client and server added]

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserDashboard from './components/UserDashboard';
+
+function App() {
+  return (
+    <div className="">
+      <UserDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/UserDashboard.js
@@ -0,0 +1,9 @@
+import Dashboard from "./dashboard/Dashboard";
+
+function UserDashboard() {
+  return (
+    <Dashboard />
+  )
+}
+
+export default UserDashboard

1552e683d1eaa8200b93ea494c26faa8532949fe
Pavan Sriram <pavangud9@gmail.com>
2022-04-10 22:59:13 +0530

    Auto stash before merge of "main" and "origin/main"

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,10 @@ import MyProduct from "./components/seller/sellerDashboard/MyProducts/MyProducts
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path='/seller/dashboard' element={<SellerDashboard />} />
-      <Route exact path='/seller/myproducts' element={<MyProduct />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route exact path='/' component={SellerDashboard} />
+      </Routes>
+    </Router>
   );
 }
 

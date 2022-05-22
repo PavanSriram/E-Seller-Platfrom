865661c6a281c8a27a598a58f71a1f12d746e16b
PavanSriram <pavangud9@gmail.com>
2022-05-22 16:29:45 +0530

    image problem

--- client/src/components/user/ItemCard.js
+++ client/src/components/user/ItemCard.js
@@ -2,29 +2,32 @@ import React from "react";
 import { useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
 
 function ItemCard(props) {
-
   const navigate = useNavigate();
 
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState([]);
 
   useEffect(() => {
     // console.log(props.child.productName);
     // let request = { productName: props.child.productName, brand: props.child.brand, sellerId: props.child.sellerId };
     // console.log(request);
-  //   // console.log("Hello");
+    //   // console.log("Hello");
     async function fetchData() {
       await axios
-        .get(`http://localhost:3308/getimage/${props.child.sellerId}/${props.child.brand}/${props.child.productName}`)
+        .get(
+          `http://localhost:3308/getimage/${props.child.sellerId}/${props.child.brand}/${props.child.productName}`
+        )
         .then((res) => {
           console.log(image);
           console.log(res.data);
@@ -44,45 +47,101 @@ function ItemCard(props) {
       discountId: props.child.discountId,
     };
 
-    await axios.put(`http://localhost:3308/addToCart/${props.user}`, request).then((res) => {
-      console.log("HELLLO!!!");
-      console.log(res);
-    });
-  }
+    await axios
+      .put(`http://localhost:3308/addToCart/${props.user}`, request)
+      .then((res) => {
+        console.log("HELLLO!!!");
+        console.log(res);
+      });
+  };
 
   const toProductPage = () => {
-    navigate("/user/product", {state: { item: props, image: image }});
-  }
+    navigate("/user/product", { state: { item: props, image: image } });
+  };
 
   return (
     <Grid item xs={12} md={4} lg={3} spacing={3}>
       <Card sx={{ maxWidth: 345, maxHeight: 350 }}>
-      
-      <img src={`${image}`} maxHeight="100" height="200" width="230"/>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {image.map((img) => (
+              <div className="carousel-item">
+                {/* <img src={img} className="d-block w-100"/> */}
+                <img
+                  src={`${img}`}
+                  className="d-block w-100"
+                  maxHeight="100"
+                  height="200"
+                  width="230"
+                  alt="dummyImage"
+                />
+              </div>
+            ))}
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon carIcon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon carIcon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
 
-      {/* <CardMedia
+        {/* <CardMedia
         component="img"
         alt="green iguana"
         height="140"
         image={URL.createObjectURL(`${image}`)}
       /> */}
 
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.child.productName.length > 15 ? props.child.productName.slice(0, 15) + "..." : props.child.productName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Price: {props.child.price}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Discount: {props.child.discountId}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={() => {toProductPage()}}>More</Button>
-        <Button size="small" onClick={handleAddToCart}>Add to Cart</Button>
-      </CardActions>
-    </Card>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.child.productName.length > 15
+              ? props.child.productName.slice(0, 15) + "..."
+              : props.child.productName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Price: {props.child.price}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Discount: {props.child.discountId}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              toProductPage();
+            }}
+          >
+            More
+          </Button>
+          <Button size="small" onClick={handleAddToCart}>
+            Add to Cart
+          </Button>
+        </CardActions>
+      </Card>
     </Grid>
   );
 }

a8a9fbbdf0a85cde365e3a32d979a2212fe2d538
Kishor783 <cs19b014@iittp.ac.in>
2022-05-22 16:30:51 +0530

    images work completed

--- client/src/components/seller/MyProducts/addProduct.js
+++ client/src/components/seller/MyProducts/addProduct.js
@@ -376,7 +376,8 @@ export default function AddProduct(props) {
                 {
                   values.images.map((img) => <div className="carousel-item">
                   <img src={img} className="d-block w-100" alt={buyImage}/>
-                </div>)}
+                </div>)
+                }
               </div>
               <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon carIcon" aria-hidden="true"></span>
